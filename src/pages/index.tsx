27a11607a96d2b53d0f1dfe2ba4adab8b2b1bd42
Daniel Marques <daniel.brz2009@gmail.com>
2023-11-07 02:10:52 -0300

    Daniel/toki 23 add short and long breaks design (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,68 @@
 import { useSounds } from "@/lib/use-sounds"
 import { Box, Button, Center, Flex, Text } from "@mantine/core"
 import {
+  IconChevronLeft,
+  IconChevronRight,
   IconPlayerPauseFilled,
   IconPlayerPlayFilled,
 } from "@tabler/icons-react"
 import NextHead from "next/head"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useTimer } from "react-timer-hook"
 
-const addZeroBefore = (time: number) => ("0" + time.toString()).slice(-2)
+function add_zero_before(time: number) {
+  return ("0" + time.toString()).slice(-2)
+}
+
+type Activity = "pomodoro" | "short_break" | "long_break"
+
+function change_activity(
+  current: Activity,
+  direction_clicked: "left" | "right",
+): Activity {
+  switch (current) {
+    case "pomodoro":
+      return direction_clicked === "left" ? "long_break" : "short_break"
+    case "short_break":
+      return direction_clicked === "left" ? "pomodoro" : "long_break"
+    case "long_break":
+      return direction_clicked === "left" ? "short_break" : "pomodoro"
+  }
+}
+
+function change_timer(activity: Activity) {
+  switch (activity) {
+    case "pomodoro":
+      return get_timer()
+    case "short_break":
+      return get_timer(60 * 5)
+    case "long_break":
+      return get_timer(60 * 15)
+  }
+}
+
+function formatActivity(activity: Activity) {
+  switch (activity) {
+    case "pomodoro":
+      return "Pomodoro"
+    case "short_break":
+      return "Short Break"
+    case "long_break":
+      return "Long Break"
+  }
+}
+
+function get_timer(time: number = 60 * 25) {
+  const timer = new Date()
+  timer.setSeconds(timer.getSeconds() + time) // 60 seconds * 25 = 25 minutes
+  return timer
+}
 
 export default function Home() {
-  const time = new Date()
-  // const time = useMemo(() => new Date(), [])
-  time.setSeconds(time.getSeconds() + 60 * 25) // 60 seconds * 25 = 25 minutes
+  const [activity, setActivity] = useState<Activity>("pomodoro")
 
   const { minutes, seconds, isRunning, pause, resume, restart } = useTimer({
-    expiryTimestamp: time,
+    expiryTimestamp: get_timer(),
     autoStart: false,
   })
 
@@ -25,14 +71,14 @@ export default function Home() {
   useEffect(() => {
     if (minutes === 0 && seconds === 0) {
       playAlarmSound()
-      restart(time, false)
+      restart(get_timer(), false)
     }
-  }, [minutes, seconds, restart, time, playAlarmSound])
+  }, [minutes, seconds, restart, playAlarmSound])
 
   return (
     <>
       <NextHead>
-        <title>{`${addZeroBefore(minutes)}:${addZeroBefore(
+        <title>{`${add_zero_before(minutes)}:${add_zero_before(
           seconds,
         )} - Toki`}</title>
       </NextHead>
@@ -46,9 +92,39 @@ export default function Home() {
         >
           <Box h={80} />
 
-          <Text c="gray.7" size="64px" fw="bold">
-            {addZeroBefore(minutes)} : {addZeroBefore(seconds)}
-          </Text>
+          <Flex align="center" gap={40}>
+            <IconChevronLeft
+              color="#909296"
+              cursor="pointer"
+              onClick={() => {
+                const next_activity = change_activity(activity, "left")
+
+                setActivity(next_activity)
+                restart(change_timer(next_activity), false)
+              }}
+            />
+
+            <Flex direction="column" align="center" gap={10}>
+              <Text>{formatActivity(activity)}</Text>
+
+              <Text c="gray.7" size="64px" fw="bold">
+                {add_zero_before(minutes)} : {add_zero_before(seconds)}
+              </Text>
+
+              <Box h={20} />
+            </Flex>
+
+            <IconChevronRight
+              color="#909296"
+              cursor="pointer"
+              onClick={() => {
+                const next_activity = change_activity(activity, "right")
+
+                setActivity(next_activity)
+                restart(change_timer(next_activity), false)
+              }}
+            />
+          </Flex>
 
           <Button
             onClick={() => {
