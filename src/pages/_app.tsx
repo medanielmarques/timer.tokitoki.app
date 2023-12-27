871f57cd346ee0ebf62982d8f4ac551cc84b4496
Daniel Marques <daniel.brz2009@gmail.com>
2023-12-27 19:09:35 -0300

    daniel/toki-49-infra-analytics-and-logging (#18)
    * add highlight.io integration
    
    * Add PostHog integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,51 @@
 import { LocalStorageSettingsProvider } from "@/lib/use-local-storage-settings"
 import { api } from "@/utils/api"
 import { supabase } from "@/utils/supabase"
+import { HighlightInit } from "@highlight-run/next/client"
 import { SessionContextProvider } from "@supabase/auth-helpers-react"
 import { type AppType } from "next/app"
 import { Montserrat } from "next/font/google"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
 
 import "../globals.css"
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug()
+    },
+  })
+}
 
 const montserrat = Montserrat({ subsets: ["latin"] })
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={montserrat.className}>
-      <SessionContextProvider supabaseClient={supabase}>
-        <LocalStorageSettingsProvider>
-          <Component {...pageProps} />
-        </LocalStorageSettingsProvider>
-      </SessionContextProvider>
-    </main>
+    <PostHogProvider>
+      {process.env.NODE_ENV === "development" && (
+        <HighlightInit
+          projectId={process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
+          serviceName={process.env.NEXT_PUBLIC_HIGHLIGHT_SERVICE_NAME}
+          tracingOrigins
+          networkRecording={{
+            enabled: true,
+            recordHeadersAndBody: true,
+            urlBlocklist: [],
+          }}
+        />
+      )}
+
+      <main className={montserrat.className}>
+        <SessionContextProvider supabaseClient={supabase}>
+          <LocalStorageSettingsProvider>
+            <Component {...pageProps} />
+          </LocalStorageSettingsProvider>
+        </SessionContextProvider>
+      </main>
+    </PostHogProvider>
   )
 }
 
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,55 @@
 import { env } from "@/env.mjs"
 import { appRouter } from "@/server/api/root"
 import { createTRPCContext } from "@/server/api/trpc"
+import { H, Handlers } from "@highlight-run/node"
+import { type TRPCError } from "@trpc/server"
 import { createNextApiHandler } from "@trpc/server/adapters/next"
+import { type NextApiRequest } from "next"
+
+const isProduction = process.env.NODE_ENV === "production"
+
+isProduction &&
+  H.init({
+    tracingOrigins: ["http://localhost:3000", "https://tokitoki.app"],
+    networkRecording: {
+      enabled: true,
+      recordHeadersAndBody: true,
+    },
+    projectID: process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID!,
+    serviceName: process.env.NEXT_PUBLIC_HIGHLIGHT_SERVICE_NAME!,
+    environment: process.env.NODE_ENV,
+  })
+
+function devLogging(error: TRPCError, path: string | undefined) {
+  console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`)
+}
+
+async function productionLogging(error: TRPCError, req: NextApiRequest) {
+  await Handlers.trpcOnError(
+    { error, req },
+    {
+      projectID: process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID!,
+      serviceName: process.env.NEXT_PUBLIC_HIGHLIGHT_SERVICE_NAME!,
+      serviceVersion: "git-sha",
+      environment: process.env.NODE_ENV,
+    },
+  )
+}
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          )
-        }
-      : undefined,
+  onError: ({ path, error, req }) => {
+    switch (env.NODE_ENV) {
+      case "production":
+        productionLogging(error, req)
+          .then(() => {})
+          .catch(() => {})
+
+      default:
+        devLogging(error, path)
+        break
+    }
+  },
 })
