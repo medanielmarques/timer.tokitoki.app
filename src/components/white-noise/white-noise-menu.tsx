740dac249eb86d52b1042156c97edfa7cc960363
Daniel Marques <daniel.brz2009@gmail.com>
2023-12-31 21:57:09 -0300

    Refactor white noise menu component

--- src/components/white-noise/white-noise-menu.tsx
+++ src/components/white-noise/white-noise-menu.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { Switch } from "@/components/ui/switch"
 import {
   Tooltip,
@@ -32,8 +32,8 @@ export function WhiteNoiseMenu() {
 
   const isWhiteNoiseMenuOpen = useIsWhiteNoiseMenuOpen()
   const isWhiteNoiseEnabled = useIsWhiteNoiseEnabled()
-
   const volume = useWhiteNoiseVolume()
+
   const {
     setIsWhiteNoiseMenuOpen,
     handleOnCheckedChange,
@@ -41,61 +41,67 @@ export function WhiteNoiseMenu() {
     increaseVolume,
   } = useWhiteNoiseActions()
 
+  const switchText = isWhiteNoiseEnabled ? "ON" : "OFF"
+
   return (
-    <DropdownMenu
-      open={isWhiteNoiseMenuOpen}
-      onOpenChange={setIsWhiteNoiseMenuOpen}
-    >
-      <DropdownMenuTrigger className="text-gray-500 hover:text-accent-foreground">
+    <Sheet open={isWhiteNoiseMenuOpen} onOpenChange={setIsWhiteNoiseMenuOpen}>
+      <SheetTrigger className="text-gray-500 hover:text-accent-foreground">
         <Headphones className="h-6 w-6 md:h-6 md:w-6" />
-      </DropdownMenuTrigger>
+      </SheetTrigger>
 
-      <DropdownMenuContent className="px-2 text-center">
-        <DropdownMenuLabel className="flex items-center justify-center gap-3">
-          <Switch
-            checked={isWhiteNoiseEnabled}
-            onCheckedChange={() => handleOnCheckedChange()}
-          />
+      <SheetContent side="bottom" className="flex flex-col">
+        <SheetHeader className="flex items-center  border-b-[1px]">
+          <SheetTitle className="mb-4 gap-2 text-2xl font-bold flex-center">
+            <span>White Noise</span>
 
-          <p>White Noise</p>
+            <TooltipProvider delayDuration={300}>
+              <Tooltip>
+                <TooltipTrigger className="mt-1">
+                  <Info className="h-5 w-5 text-gray-600" />
+                </TooltipTrigger>
 
-          <TooltipProvider delayDuration={300}>
-            <Tooltip>
-              <TooltipTrigger>
-                <Info className="h-5 w-5 text-gray-600" />
-              </TooltipTrigger>
+                <TooltipContent>
+                  <span>White noise only plays when the timer is active</span>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </SheetTitle>
+        </SheetHeader>
 
-              <TooltipContent>
-                <p>White noise only plays when the timer is active</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </DropdownMenuLabel>
+        <div className="gap-5 py-4 flex-center">
+          <div className="gap-3 flex-center">
+            <span className="font-bold">{switchText}</span>
+            <Switch
+              checked={isWhiteNoiseEnabled}
+              onCheckedChange={() => handleOnCheckedChange()}
+            />
+          </div>
 
-        <DropdownMenuSeparator />
+          <div className="gap-4 flex-center">
+            <Button
+              variant="ghost"
+              className="w-13 h-10 font-medium text-gray-600"
+              onClick={decreaseVolume}
+              disabled={volume === 0}
+            >
+              <SpeakerSimpleLow className="h-5 w-5" />
+            </Button>
 
-        <div className="flex items-center justify-between gap-4">
-          <Button
-            variant="ghost"
-            className="w-13 h-10 font-medium text-gray-600"
-            onClick={decreaseVolume}
-            disabled={volume === 0}
-          >
-            <SpeakerSimpleLow className="h-5 w-5" />
-          </Button>
+            <span className="text-lg font-semibold text-gray-600">
+              {volume}
+            </span>
 
-          <span className="text-lg font-semibold text-gray-600">{volume}</span>
-
-          <Button
-            variant="ghost"
-            className="w-13 h-10 font-medium text-gray-600"
-            onClick={increaseVolume}
-            disabled={volume === 10}
-          >
-            <SpeakerSimpleHigh className="h-5 w-5" />
-          </Button>
+            <Button
+              variant="ghost"
+              className="w-13 h-10 font-medium text-gray-600"
+              onClick={increaseVolume}
+              disabled={volume === 10}
+            >
+              <SpeakerSimpleHigh className="h-5 w-5" />
+            </Button>
+          </div>
         </div>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </SheetContent>
+    </Sheet>
   )
 }
