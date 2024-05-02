6122d602c90090189485b9d006364584c17f915c
Daniel Marques <daniel.brz2009@gmail.com>
2024-05-02 17:41:39 -0300

    revert homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function LandingPage() {
-  return <div className="flex-center">Landing Page Goes Here 😆</div>
+import { CommandCenter } from "@/components/command-center/command-center"
+import { Header } from "@/components/header"
+import { TimerSection } from "@/components/timer/timer"
+import { useCountdown } from "@/components/timer/timer-utils"
+
+export default function TimerPage() {
+  useCountdown()
+
+  return (
+    <>
+      <div className=" flex h-screen justify-center md:items-start">
+        <div className="flex w-[600px] flex-col justify-between md:justify-start">
+          <div className="flex h-screen flex-col justify-between">
+            <div>
+              <Header />
+              <TimerSection />
+            </div>
+
+            <div className="mb-8 hidden md:flex-center">
+              <CommandCenter />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
